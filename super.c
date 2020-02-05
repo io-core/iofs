15c38d50387c1c6fe9770d352ce3aa6334c76743
Charles Perkins <charlesap@gmail.com>
2020-02-05 09:38:08 -0800

    remove commented-out code

--- super.c
+++ super.c
@@ -25,55 +25,6 @@ static int iofs_fill_super(struct super_block *sb, void *data, int silent) {
     }
 release:
     return ret;
-
-/*
-    struct inode *root_inode;
-    struct iofs_inode *root_iofs_inode;
-    struct buffer_head *bh;
-    struct iofs_superblock *iofs_sb;
-    int ret = 0;
-
-    bh = sb_bread(sb, IOFS_SUPERBLOCK_BLOCK_NO);
-    BUG_ON(!bh);
-    iofs_sb = (struct iofs_superblock *)bh->b_data;
-    if (unlikely(iofs_sb->magic != IOFS_MAGIC)) {
-        printk(KERN_ERR
-               "The filesystem being mounted is not of type iofs. "
-               "Magic number mismatch: %llu != %llu\n",
-               iofs_sb->magic, (uint64_t)IOFS_MAGIC);
-        goto release;
-    }
-    if (unlikely(sb->s_blocksize != iofs_sb->blocksize)) {
-        printk(KERN_ERR
-               "iofs seem to be formatted with mismatching blocksize: %lu\n",
-               sb->s_blocksize);
-        goto release;
-    }
-
-    sb->s_magic = iofs_sb->magic;
-    sb->s_fs_info = iofs_sb;
-    sb->s_maxbytes = iofs_sb->blocksize;
-    sb->s_op = &iofs_sb_ops;
-
-    root_iofs_inode = iofs_get_iofs_inode(sb, IOFS_ROOTDIR_INODE_NO);
-    root_inode = new_inode(sb);
-    if (!root_inode || !root_iofs_inode) {
-        ret = -ENOMEM;
-        goto release;
-    }
-    iofs_fill_inode(sb, root_inode, root_iofs_inode);
-    inode_init_owner(root_inode, NULL, root_inode->i_mode);
-
-    sb->s_root = d_make_root(root_inode);
-    if (!sb->s_root) {
-        ret = -ENOMEM;
-        goto release;
-    }
-
-release:
-    brelse(bh);
-    return ret;
-*/
 }
 
 struct dentry *iofs_mount(struct file_system_type *fs_type,
