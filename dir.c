8359fe106a205a523daf8288161241321321f235
Charles Perkins <charlesap@gmail.com>
2020-02-08 06:50:05 -0800

    return ctx->pos that allows dir scan to complete

--- dir.c
+++ dir.c
@@ -32,92 +32,42 @@ static int iofs_readdir(struct file *file, struct dir_context *ctx)
 	struct iofs_dinode	*dinode;
 	struct iofs_de		*dirslot;
 
-		bh = sb_bread(inode->i_sb, 0); //iofs_bmap(inode, block));
+	bh = sb_bread(inode->i_sb, 0); //iofs_bmap(inode, block));
 
-		if (!bh) {
-			pr_err("%s(): failed to read dir inode %d\n",
-			       __func__, 29);
-			
-		}
-
-		dinode = (struct iofs_dinode *) bh->b_data; 
-/*
-		if (be16_to_cpu(dirblock->magic) != IOFS_DIRBLK_MAGIC) {
-			pr_err("%s(): invalid directory block\n", __func__);
-			brelse(bh);
-			break;
-		}
-
-		for (; slot < dirblock->slots; slot++) {
-			struct iofs_dentry *dirslot;
-			iofs_ino_t inodenum;
-			const char *nameptr;
-			int namelen;
-
-			if (dirblock->space[slot] == 0)
-				continue;
-
-			dirslot  = (struct iofs_dentry *) (((char *) bh->b_data) + IOFS_SLOTAT(dirblock, slot));
-
-			inodenum = be32_to_cpu(dirslot->inode);
-			namelen  = dirslot->namelen;
-			nameptr  = dirslot->name;
-			pr_debug("%s(): block %d slot %d/%d: inode %u, name \"%s\", namelen %u\n",
-				 __func__, block, slot, dirblock->slots-1,
-				 inodenum, nameptr, namelen);
-			if (!namelen)
-				continue;
-			// found the next entry 
-			ctx->pos = (block << IOFS_DIRBSIZE_BITS) | slot;
-
-			// sanity check 
-			if (nameptr - (char *) dirblock + namelen > IOFS_DIRBSIZE) {
-				pr_warn("directory entry %d exceeds directory block\n",
-					slot);
-				continue;
-			}
-
-			// copy filename and data in dirslot 
-			if (!dir_emit(ctx, nameptr, namelen, inodenum, DT_UNKNOWN)) {
-				brelse(bh);
-				return 0;
-			}
-		}
-		brelse(bh);
-
-		slot = 0;
-		block++;
+	if (!bh) {
+		pr_err("%s(): failed to read dir inode %d\n",
+		       __func__, 29);
+		
 	}
 
-	ctx->pos = (block << IOFS_DIRBSIZE_BITS) | slot;
-*/
+	dinode = (struct iofs_dinode *) bh->b_data; 
 
 
-		if (le32_to_cpu(dinode->origin) != IOFS_DIRMARK) {
-			pr_err("%s(): invalid directory inode\n", __func__);
-			brelse(bh);
-			return 0;
-		}
+	if (le32_to_cpu(dinode->origin) != IOFS_DIRMARK) {
+		pr_err("%s(): invalid directory inode\n", __func__);
+		brelse(bh);
+		return 0;
+	}
 
-		for (slot = ctx->pos; slot < dinode->dirb.m && slot < 24; slot++) {
-			dirslot  = &dinode->dirb.e[slot];
-			namelen  = strnlen(dirslot->name,24);
-			nameptr  = dirslot->name;
+	for (slot = ctx->pos; slot < dinode->dirb.m && slot < 24; slot++) {
+		dirslot  = &dinode->dirb.e[slot];
+		namelen  = strnlen(dirslot->name,24);
+		nameptr  = dirslot->name;
 
-                        pr_debug("%s(): slot %d name \"%s\", namelen %u\n",
-                                 __func__, slot, 
-                                 nameptr, namelen);
-	                ctx->pos = slot;
-	                if (!dir_emit(ctx, nameptr, namelen, dirslot->adr, DT_UNKNOWN)) {
-	                        brelse(bh);
-	                        return 0;
-	                }
+                pr_debug("%s(): slot %d name \"%s\", namelen %u\n",
+                         __func__, slot, 
+                         nameptr, namelen);
+                ctx->pos = slot;
+                if (!dir_emit(ctx, nameptr, namelen, dirslot->adr, DT_UNKNOWN)) {
+                        brelse(bh);
+                        return 0;
+                }
 
-		}
+	}
 
 
         brelse(bh);
-        ctx->pos = 0;
+        ctx->pos = dinode->dirb.m;
 
 	return 0;
 }
